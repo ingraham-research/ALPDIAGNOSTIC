a20b53d4f88807e3c328581fb5064cf66be39e1a
Mia Onodera <69021953+onoderamia@users.noreply.github.com>
2025-05-20 22:13:29 -0700

    Update server.js

--- baby-diagnostics/backend/server.js
+++ baby-diagnostics/backend/server.js
@@ -15,7 +15,7 @@ const {
 const app = express();
 
 app.use(cors({
-  origin: 'https://alpdiagnostic.onrender.com', // CHANGE HERE
+  origin: 'https://alpdiagnostic.vercel.app', // CHANGE HERE
   methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
   credentials: true
